38597790ea453c8a03f23a5d6b17b19f678ab41b
Max Ivanov <waxivanov@gmail.com>
2020-07-01 11:51:27 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	$('.menu-burger').click(function(event) {
 		$('.menu-burger').toggleClass('active');
+		$('.menu').toggleClass('active');
+		$('body').toggleClass('lock');
 	});
 });
